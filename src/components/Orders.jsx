dd2c5a6dea5e4b0db75ad892b1988577f76ef4ee
lek <abungan082198@gmail.com>
2025-06-12 16:50:53 +0800

    updated

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -19,7 +19,7 @@ export default function Orders() {
           <thead>
             <tr>
               <th>Order No.</th>
-              <th>Items</th>
+              <th>Dessrts</th>
               <th>Total Price</th>
               <th>Action</th>
             </tr>
@@ -27,23 +27,25 @@ export default function Orders() {
           <tbody>
             {orders.map((order, index) => (
               <tr key={index} className="border-bottom no">
-                <td data-cell="Order No. " className="text-center">
-                  {index + 1}
-                </td>
+                <td data-cell="Order No. ">{index + 1}</td>
                 <td>
                   <table>
                     <tbody>
                       {order.item.map((food, i) => (
-                        <tr className="tr-flex" key={i}>
-                          <td className="td-items">{food.name}</td>
-                          <td className="td-bold">x {food.quantity + 1}</td>
+                        <tr key={i}>
+                          <td className="tr-flex">
+                            <span>{food.name}</span>
+                            <span className="end">x{food.quantity + 1}</span>
+                          </td>
                         </tr>
                       ))}
                     </tbody>
                   </table>
                 </td>
-                <td className="text-center td-items">${order.totalPrice}</td>
-                <td className="text-center">
+                <td data-cell="Total Price: " className="text-center td-items">
+                  ${order.totalPrice}
+                </td>
+                <td className="text-center td-items">
                   <button className="btn" onClick={() => cancelOrder(index)}>
                     Cancel Order
                   </button>
